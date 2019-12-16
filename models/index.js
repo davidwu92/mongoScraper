0f43c847714a6ccc1547491f7f042a2e7f541d9e
David Wu <davidjwu92@gmail.com>
2019-12-16 08:46:33 -0800

    small notes

--- models/index.js
+++ models/index.js
@@ -2,6 +2,7 @@
 
 const {model, Schema} = require('mongoose')
 
+//Whenever a model is created, don't forget to require and export here.
 const Article = require('./Article.js')(model, Schema)
 
 module.exports = {Article}
--- routes/index.js
+++ routes/index.js
@@ -2,4 +2,7 @@
 
 module.exports = app => {
   require('./articleRoutes')(app) //uncertain if keeping route
+  
+  //any additional models will need routes required here.
+
 }
